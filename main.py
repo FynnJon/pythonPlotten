7b505b61386efba2953a1ad44fd8fa62041df3f8
FynnJon <fynn.kunzmann@gmx.de>
2022-05-24 22:14:32 +0200

    Erster Test

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Fynn')
+    print("How are you?")
+    print("Meddl")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
