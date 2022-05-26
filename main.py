8901a0aee3deb5c6f1dd24f529d80653c94f4720
FynnJon <fynn.kunzmann@gmx.de>
2022-05-26 21:43:29 +0200

    zweite aufgabe anfang

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ mpl.use('TkAgg')
 
 data = pd.read_csv('e7_1.csv', sep=';')
 
-x_data = data['I in mA']
-y_data = data['U in V']
-x_err = data['I error']
-y_err = data['U error']
+#x_data = data['I in mA']
+#y_data = data['U in V']
+#x_err = data['I error']
+#y_err = data['U error']
+x_data = data['Ohm']
+y_data = data['P in W']
+x_err = data['Ohm']*0.002
+y_err = data['P error']
 
-lin = linregress(x_data, y_data)
-print(lin)
+#lin = linregress(x_data, y_data)
+#print(lin)
 
-plt.errorbar(x_data, y_data, y_err, x_err, fmt=',', linewidth=1, capsize=2)
-plt.plot(x_data, lin.intercept + lin.slope*x_data)
+plt.errorbar(x_data, y_data, y_err, x_err, fmt=',', linewidth=0.8, capsize=1.5)
+#plt.plot(x_data, lin.intercept + lin.slope*x_data)
 
 plt.xlabel(r'Strom $I$ in mA')
 plt.ylabel(r'Spannung $U$ in V')
