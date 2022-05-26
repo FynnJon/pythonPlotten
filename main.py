90b65019b39b1993e661d93b05057871212eef90
FynnJon <fynn.kunzmann@gmx.de>
2022-05-26 19:43:25 +0200

    Leerzeilen aus csv entfernt

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ y_data = data['U in V']
 x_err = data['I error']
 y_err = data['U error']
 
+lin = linregress(x_data, y_data)
+print(lin)
+
 plt.errorbar(x_data, y_data, y_err, x_err, fmt=',', linewidth=1, capsize=2)
+plt.plot(x_data, lin.intercept + lin.slope*x_data)
+
+plt.xlabel(r'Strom $I$ in mA')
+plt.ylabel(r'Spannung $U$ in V')
 
 plt.show()
