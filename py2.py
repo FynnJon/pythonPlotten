60efca3bf61c888c9119477e31ffc8e29f5bd692
FynnJon <fynn.kunzmann@gmx.de>
2022-05-28 15:39:39 +0200

    e7 aufgabe 2

--- py2.py
+++ py2.py
@@ -7,10 +7,20 @@ from scipy.stats import linregress
 
 mpl.use('TkAgg')
 
-data = pd.read_csv('e7_1.csv', sep=';')
+data = pd.read_csv('e7_2d.csv', sep=';')
 
-x = np.arange(0.0, 3.0, 0.01)
+x_data = data['R_AC/R_AB']
+x_err = data['poti err']
+y1_data = data['U_L/U_0(1)']
+y2_data = data['u/u(2)']
+y3_data = data['u/u(3)']
+y4_data = data['u/u(4)']
+y1_err = data['u/u err(1)']
+y2_err = data['u/u err(2)']
+y3_err = data['u/u err(3)']
+y4_err = data['u/u err(4)']
 
+x = np.arange(0.0, 1.0, 0.01)
 y1 = x
 y2 = x/(((x*(1-x))/5)+1)
 y3 = x/(((x*(1-x))/0.5)+1)
@@ -19,10 +29,14 @@ y4 = x/(((x*(1-x))/0.05)+1)
 a = y3 > 0
 b = y4 > 0
 
-plt.plot(x, y1, color='red')
-plt.plot(x, y2, color='blue')
-plt.plot(x[a], y3[a], color='orange')
-plt.plot(x[b], y4[b], color='green')
+plt.plot(x, y1, color='red', linewidth=0.7)
+plt.errorbar(x_data, y1_data, y1_err, x_err, fmt=',', linewidth=0.7, capsize=2, color='red')
+plt.plot(x, y2, color='blue', linewidth=0.7)
+plt.errorbar(x_data, y2_data, y2_err, x_err, fmt=',', linewidth=0.7, capsize=2, color='blue')
+plt.plot(x[a], y3[a], color='orange', linewidth=0.7)
+plt.errorbar(x_data, y3_data, y3_err, x_err, fmt=',', linewidth=0.7, capsize=2, color='orange')
+plt.plot(x[b], y4[b], color='green', linewidth=0.7)
+plt.errorbar(x_data, y4_data, y4_err, x_err, fmt=',', linewidth=0.7, capsize=2, color='green')
 plt.axis([0, 1, 0, 1])
 plt.grid()
 plt.xlabel(r"$\frac{R_{AC}}{R_{AB}}$")
