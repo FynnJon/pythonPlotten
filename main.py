acedfe80130c3cf0ff8e5710e1747e9e2bcb3cf9
FynnJon <fynn.kunzmann@gmx.de>
2022-06-16 19:51:57 +0200

    a12

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ from scipy.stats import linregress
 
 mpl.use('TkAgg')
 
-data = pd.read_csv('e7_1.csv', sep=';')
+data = pd.read_csv('a12_3.csv', sep=';')
 
-#x_data = data['I in mA']
-#y_data = data['U in V']
-#x_err = data['I error']
-#y_err = data['U error']
+x_data = data['d in mm']
+y_data = data['rate_korr']
+x_err = data['d_err']
+y_err = data['rate_err']
 
-x_data = data['R in Ohm']
-y_data = data['P in mW']
-x_err = data['R error']
-y_err = data['P err in mW']
+lin = linregress(x_data, np.log(y_data))
+print(lin)
 
-#lin = linregress(x_data. y_data)
-#print(lin)
+#print(fit)
 
 plt.errorbar(x_data, y_data, y_err, x_err, fmt=',', linewidth=0.8, capsize=1.5)
-#plt.plot(x_data. lin.intercept + lin.slope*x_data)
+plt.plot(x_data, np.exp(lin.intercept)*np.exp(lin.slope*x_data), label=r'$7.954e^{-0.688d}$')
 
 plt.grid()
-plt.xlabel(r'Widerstand $R$ in $\Omega$')
-plt.ylabel(r'Leistung $P$ in mW')
+plt.axis([0, 5, 0, 10])
+plt.xlabel(r'Papierdicke $d$ in mm')
+plt.ylabel(r'Zählrate $Z$ in 1/s')
+plt.legend()
 
 plt.show()
