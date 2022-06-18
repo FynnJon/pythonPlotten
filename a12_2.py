8a9248797ad1c5343e723616668d16a7f787105a
FynnJon <fynn.kunzmann@gmx.de>
2022-06-18 12:09:56 +0200

    a12 c

--- a12_2.py
+++ a12_2.py
@@ -11,12 +11,28 @@ data1 = pd.read_csv('a12_2.csv', sep='	')
 data2 = pd.read_csv('a12_2b.csv', sep='	')
 x1 = data1['anzahl1']
 x2 = data2['anzahl1']
+xs1 = np.std(x1)
+xs2 = np.std(x2)
+xm1 = np.mean(x1)
+xm2 = np.mean(x2)
+x = np.arange(0, 35, 0.1)
 
-plt.hist(x1, bins=9, edgecolor='white')
-plt.hist(x2, bins=24, edgecolor='white')
+print(xs1)
+print(xm1)
+print(xs2)
+print(xm2)
+
+p = xm1**x/np.math.factorial(x)*np.exp(-xm1)
+np.sum()
+
+plt.hist(x1, bins=9, density=True, edgecolor='white')
+plt.hist(x2, bins=24, density=True, edgecolor='white')
+#plt.plot(x, np.exp(-xm1)*xm1**x/np.math.factorial(x), '--')
+plt.plot(x, 1/(np.sqrt(2*np.pi)*xs2)*np.exp(-((x-xm2)**2)/(2*xs2**2)), '--', color='red')
+
 
 plt.xlabel(r'Zählrate in 1/s')
-plt.ylabel(r'Häufigkeit')
-plt.axis([0, 30, 0, 140])
+plt.ylabel(r'Relative Häufigkeit')
+plt.axis([0, 30, 0, 0.31])
 
 plt.show()
--- main.py
+++ main.py
@@ -14,13 +14,16 @@ y_data = data['rate_korr']
 x_err = data['d_err']
 y_err = data['rate_err']
 
-lin = linregress(x_data, np.log(y_data))
-print(lin)
-
-#print(fit)
+#lin = linregress(x_data, np.log(y_data))
+#print(lin)
+A = 8.34729
+l = 1/1.183561
+offset = 0.3496887
+x = np.arange(0, 6, 0.2)
 
 plt.errorbar(x_data, y_data, y_err, x_err, fmt=',', linewidth=0.8, capsize=1.5)
-plt.plot(x_data, np.exp(lin.intercept)*np.exp(lin.slope*x_data), label=r'$7.954e^{-0.688d}$')
+#plt.plot(x_data, np.exp(lin.intercept)*np.exp(lin.slope*x_data), label=r'$7.954e^{-0.688d}$')
+plt.plot(x, offset+A*np.exp(-l*x), label=r'$8.35e^{-0.84d}+0.35$')
 
 plt.grid()
 plt.axis([0, 5, 0, 10])
