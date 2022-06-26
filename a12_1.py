4624f0f42c666125e2dd460103b760401fe7fadb
FynnJon <fynn.kunzmann@gmx.de>
2022-06-26 18:36:34 +0200

    a_12 sfhdfsdas

--- a12_1.py
+++ a12_1.py
@@ -18,19 +18,30 @@ nullrate = np.mean(y_nr)
 err = np.std(y_nr)
 print(nullrate)
 print(err)
-x_err = 0
+#x_err = x_data*0.02
 y_err = 2*err
 x = np.arange(0, 200, 1)
+y1 = 11.94*np.exp(-0.0122*(x-30.0483))-1.06+nullrate
+y2 = 0.5*np.exp(-0.001155*(x-30.0483))+nullrate
+y3 = y1-y2
 
-plt.errorbar(x_data, y_data, y_err, x_err, fmt=',', linewidth=0.8, capsize=1.5)
-plt.errorbar(x_nr, y_nr, y_err, x_err, fmt=',', linewidth=0.8, capsize=1.5)
-#plt.plot(x_data, np.exp(lin.intercept)*np.exp(lin.slope*x_data), label=r'$7.954e^{-0.688d}$')
-plt.plot(x, 0*x+nullrate)
+#Nullrate
+plt.errorbar(x_nr, y_nr, y_err, x_nr*0.02, fmt=',', linewidth=0.7, capsize=0.9, color='green')
+plt.plot(x, 0*x+nullrate, color='green', linewidth=0.7, label='Nullrate')
+#Messdaten
+plt.errorbar(x_data, y_data, y_err, x_data*0.02, fmt=',', linewidth=0.7, capsize=0.9, color='blue', label='Zerfallsmessung')
+plt.plot(x, y1, color='blue', linewidth=0.7, label=r'$f(t)=11.94e^{-0.012t}-1.06$')
+#plt.plot(x, y2)
+plt.plot(x, y3, color='orange', linewidth=0.7, label=r'$g(t)=f(t)-0.5e^{-0.001155t}$')
+plt.vlines(30.0483, 5.19, 10.38, linewidth=0.7, color='red')
+plt.text(27.5, 7.785, r'$Z_0-Z_{1/2}$', rotation='vertical', va='center', ha='center', color='red')
+plt.hlines(5.19, 30.0483, 46.958+30.0483, linewidth=0.7, color='red')
+plt.text(53.53, 4.8, r'$T_{1/2}-T_0$', va='center', ha='center', color='red')
 
-plt.grid()
+#plt.grid()
 plt.axis([0, 185, 0, 12])
-plt.xlabel(r'Zeit in min')
+plt.xlabel(r'Zeit $t$ in min')
 plt.ylabel(r'Zählrate $Z$ in Bq')
-#plt.legend()
+plt.legend()
 
 plt.show()
--- a12_5.py
+++ a12_5.py
@@ -14,16 +14,16 @@ y_data = data['rate_korr']
 x_err = data['r_k_err']
 y_err = data['rate_err']
 
-#lin = linregress(x_data, np.sqrt(1/y_data))
-#print(lin)
-x = np.arange(0, 14, 0.1)
+lin = linregress(x_data, y_data)
+print(lin)
+x = np.arange(0, 0.1, 0.1)
 
 plt.errorbar(x_data, y_data, y_err, x_err, fmt=',', linewidth=0.8, capsize=1.5)
-#plt.plot(x, (lin.intercept)^2*x)
+plt.plot(x_data, lin.slope*x_data+lin.intercept, label=r'y=254.88x-1.32')
 
-plt.grid()
-#plt.axis([0, 5, 0, 10])
-plt.xlabel(r'Abstand $1/r^2$ in 1/cm$^2$')
+#plt.grid()
+plt.axis([0, 0.03, 0, 6.1])
+plt.xlabel(r'$1/r^2$ in 1/cm$^2$')
 plt.ylabel(r'Zählrate $Z$ in Bq')
 plt.legend()
 
